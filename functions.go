b157e60dd9df9e4f4f924ea0bcc96bf1cf4aa6a2
高沢崚 <ryotakazawaengineer@gmail.com>
2020-06-06 23:38:54 +0900

    functions.go

---
+++ functions.go
@@ -0,0 +1,36 @@
+// Go において<em>関数</em>は中心的な存在だ。
+// 例を見ながら関数について学ぼう。
+
+package main
+
+import "fmt"
+
+// これは2つの `int` 型の値を受け取り、その和を表す `int` 型の値を返す関数である。
+func plus(a int, b int) int {
+
+	// Go では明示的な return 文を書く必要がある。
+	// すなわち、return を書かなければ、最後の式の値を自動で返しはしない。
+	return a + b
+}
+
+// 同じ型のパラメータを続けて複数取るとき、それらの型名はそのうち最後のものの後にだけ書けばよい。
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+
+func plusPlusPlus(a, b, c, d int) int {
+	return a + b + c + d
+}
+
+func main() {
+
+	// `name(args)` と書けば関数を呼べる
+	res := plus(1, 2)
+	fmt.Println("1+2 =", res)
+
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3 =", res)
+
+	res = plusPlusPlus(1, 2, 3, 4)
+	fmt.Println("1+2+3+4", res)
+}
