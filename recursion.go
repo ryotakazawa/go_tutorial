4830ef70320cbd3959fcd1de708e06783b628fb9
高沢崚 <ryotakazawaengineer@gmail.com>
2020-06-07 23:40:57 +0900

    recursion.go

---
+++ recursion.go
@@ -0,0 +1,19 @@
+// Go は<a href="http://en.wikipedia.org/wiki/Recursion_(computer_science)"><em>再帰関数</em></a>をサポートしている。
+// この例は古典的な階乗を計算するものだ。
+
+package main
+
+import "fmt"
+
+// この関数 `fact` は、ベースケースである `fact(0)` に到達するまで自身を繰り返し呼び出す。
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(7))
+	fmt.Println(fact(4))
+}
