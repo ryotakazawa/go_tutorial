7e197af1149f74030d76d68e2854bebb16bbe226
高沢崚 <ryotakazawaengineer@gmail.com>
2020-06-10 22:35:24 +0900

    channel_synchronization.go

---
+++ channel_synchronization.go
@@ -0,0 +1,30 @@
+// チャネルを使ってゴルーチン間を同期できる。
+// この例では、ゴルーチンが処理を終えるのを待つために、受信がブロックされることを利用する。
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// この関数をゴルーチンで実行する。
+// `done` チャネルはこの関数の仕事が終わったことを、別のゴルーチンに知らせるために使う。
+func worker(done chan bool) {
+	fmt.Print("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	// 終わったことを知らせるために値を送信する
+	done <- true
+}
+
+func main() {
+
+	// 通知用のチャネルを渡してゴルーチンを起動する。
+	done := make(chan bool, 1)
+	go worker(done)
+
+	// ワーカーからこのチャネルに通知が届くまでブロックされる。
+	<-done
+}
