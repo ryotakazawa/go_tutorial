d61d94e93d4cf762173a597d7a75e502438a1ddf
高沢崚 <ryotakazawaengineer@gmail.com>
2020-06-13 21:36:27 +0900

    non-blocking-channel-operations.go

---
+++ non-blocking-channel-operations.go
@@ -0,0 +1,45 @@
+// 基本的なチャネルの送受信の使い方はブロックする。
+// しかし、`select` と `default` 節を使えば、<em>ノンブロッキング</em>な送受信や、選択的な受信を実装できる。
+
+package main
+
+import "fmt"
+
+func main() {
+	messages := make(chan string, 1)
+	signals := make(chan bool, 1)
+
+	// これはノンブロッキングな受信である。
+	// もし `messages` から値を受け取れるなら、`select` は `<-messages` の `case` 節で、その値を受け取る。
+	// そうでなければ、`default` 節に直ちに進む。
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
+
+	// ノンブロッキングな送信も同様だ。
+	// ここでは `msg` を `messages` チャネルに送信できない。
+	// なぜならチャネルにバッファは付いておらず、受信者もいないからである。
+	// そのため、`default` 節に進む。
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+
+	// `default` 節の前に複数の `case` を書けば、複数のチャネルから選択的かつノンブロッキングに受信できる。
+	// ここでは `messages` と`signals` からノンブロッキング受信を試みている。
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+}
