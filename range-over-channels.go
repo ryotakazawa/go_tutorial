9e75aa6fad74d0169d8d5fe4f191d68213870d42
高沢崚 <ryotakazawaengineer@gmail.com>
2020-06-13 21:58:20 +0900

    range-over-channels.go

---
+++ range-over-channels.go
@@ -0,0 +1,22 @@
+// [前の例](range)で、`for` と `range` を使って基本的なデータ構造上の繰り返しが書けることを紹介した。
+// この記法を使って、チャネルから値を繰り返し受信することもできる。
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// この例では queue チャネルに送信された2つの値を処理する。
+	queue := make(chan string, 3)
+	queue <- "one"
+	queue <- "two"
+	queue <- "three"
+	close(queue)
+
+	// この `range` は `queue` から要素を繰り返し取得する。
+	// 既に上でチャネルを `close` しているので、この繰り返しは2つ要素を受信すると終わる。
+	for elem := range queue {
+		fmt.Println(elem)
+	}
+}
